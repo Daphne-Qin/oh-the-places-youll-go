35f357b7731cdcb5bd408da2c22b865a76846949
RUN <dq7@rice.edu>
2026-04-15 13:45:16 -0500

    new horton animations

--- scripts/horton.gd
+++ scripts/horton.gd
@@ -14,14 +14,12 @@ func walk_noclover(speed: float = 20.0):
 	$AnimatedSprite2D.play("walk_noclover")
 
 func idle_anxious_noclover(speed: float = 20.0):
-	# there's not a set animation yet...
-	$AnimatedSprite2D.play("walk_noclover")
-	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.sprite_frames.set_animation_speed("idle_anxious_noclover", speed)
+	$AnimatedSprite2D.play("idle_anxious_noclover")
 
 func idle_anxious_clover(speed: float = 20.0):
-	# there's not a set animation yet...
-	$AnimatedSprite2D.play("walk_clover")
-	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.sprite_frames.set_animation_speed("idle_anxious_clover", speed)
+	$AnimatedSprite2D.play("idle_anxious_clover")
 
 func idle_happy_clover(speed: float = 20.0):
 	$AnimatedSprite2D.sprite_frames.set_animation_speed("idle_happy_clover", speed)
